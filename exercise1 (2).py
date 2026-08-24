4a22e141a2cbe5ea70ca74d2e1211e7d3143eb95
pavanbhosale2007-wq <pavanbhosale2007@gmail.com>
2026-08-24 13:18:58 +0530

    Add files via upload

---
+++ exercise1 (2).py
@@ -0,0 +1,9 @@
+name = input("Enter Employee Name: ")
+role = input("Enter Employee Role: ")
+salary = float(input("Enter Monthly Salary: "))
+
+print("\n---------------")
+print("EMPLOYEE IDENTITY CARD")
+print("Name   : ",name)
+print("Role   : ", role)
+print("Salary : ₹",salary)
