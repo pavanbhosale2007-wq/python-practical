9cc6930daed0aad31b112718f9dfa3cee06dad94
pavanbhosale2007-wq <pavanbhosale2007@gmail.com>
2026-08-24 14:00:30 +0530

    Add files via upload

--- pr3ex1.py
+++ pr3ex1.py
@@ -4,7 +4,7 @@ income = float(input("Enter annual income:"))
 
 caste = input("enter your caste (SC/ST/NT/OBC/OPEN):")
 
-if age < 25 and income < 200000 and caste in ["SC" "ST" "NT" "OBC"]:
+if age < 25 and income < 300000 and caste in ["SC" "ST" "NT" "OBC"]:
     print("you are eligible for scholorship:")
 
 else:
